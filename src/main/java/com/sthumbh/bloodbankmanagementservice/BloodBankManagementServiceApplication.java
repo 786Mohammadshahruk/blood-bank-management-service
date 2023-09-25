771814fdd89939df00d258b1e7bd04271c5225a5
Mohammad Shahruk <mshahruk57@gmail.com>
2023-09-25 16:37:06 +0530

    initial commit

---
+++ src/main/java/com/sthumbh/bloodbankmanagementservice/BloodBankManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sthumbh.bloodbankmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodBankManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodBankManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sthumbh/bloodbankmanagementservice/BloodBankManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sthumbh.bloodbankmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodBankManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
